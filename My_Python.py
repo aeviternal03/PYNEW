fca1ef3cb9bae9b0ee905ba95a939312d86dba99
aeviternal03 <eraakansha3@gmail.com>
2024-12-23 22:05:58 +0530

    New features added to the game

--- My_Python.py
+++ My_Python.py
@@ -35,4 +35,65 @@ while True:
     else:
         print('Invalid choice!')
 
+#Modify the program so the user can specify how many dice they want to roll
+
+import random
+
+while True:
+
+    choice = input("Roll the dice? (y/n):  ").lower()
+    if choice ==  'y':
+        try:
+            num_dice = int(input('How many dice would you like to roll? ')) 
+            if num_dice < 1:
+                print('Please enter a positive number of dice')
+                continue
+
+            
+            die_roll = [random.randint(1,6) for _ in range(num_dice)]
+            print(f'roll {num_dice} dice: {die_roll}')
+
+        except ValueError:
+            print('Invalid input! Please enter a number')
+
+    elif choice == 'n':
+        print('Thanks for playing!')
+        break
+    else:
+        print('Invalid choice!')
+
+
+#Add a feature that keeps track of how many times the user has rolled the dice 
+#this will require a counter that increments each time the dice are rolled
+
+import random
+
+roll_count = 0
+
+while True:
+
+    choice = input("Roll the dice? (y/n):  ").lower()
+    if choice ==  'y':
+        try:
+            num_dice = int(input('How many dice would you like to roll? ')) 
+            if num_dice < 1:
+                print('Please enter a positive number of dice')
+                continue
+
+            
+            die_roll = [random.randint(1,6) for _ in range(num_dice)]
+            print(f'roll {num_dice} dice: {die_roll}')
+
+            #increment the rol count
+            roll_count += 1
+
+        except ValueError:
+            print('Invalid input! Please enter a number')
+
+    elif choice == 'n':
+        print('Thanks for playing!')
+        print(f'You rolled the dice {roll_count} times')
+        break
+    else:
+        print('Invalid choice!')
 
